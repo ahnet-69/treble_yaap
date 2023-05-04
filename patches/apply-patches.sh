af8b692903d0fa903d92fb8b37de925df88a2869
captaincookie200 <syedmusicrealise@gmail.com>
2023-05-04 18:53:57 +0500

    Add other various patches to fix build errors and issues with gsi

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,7 +4,8 @@ set -e
 
 source="$(readlink -f -- $1)"
 trebledroid="$source/patches/trebledroid"
-
+ponces="$source/patches/ponces"
+matvey="$source/patches/matvey"
 
 printf "\n ### APPLYING TREBLEDROID PATCHES ###\n";
 sleep 1.0;
@@ -17,6 +18,105 @@ for path in $(cd $trebledroid; echo *); do
 	pushd $tree
 
 	for patch in $trebledroid/$path/*.patch; do
+		# Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+            printf "### ALREDY APPLIED: $patch \n";
+			continue
+		fi
+
+		if git apply --check $patch; then
+			git am $patch
+		elif patch -f -p1 --dry-run < $patch > /dev/null; then
+			#This will fail
+			git am $patch || true
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
+		else
+			printf "### FAILED APPLYING: $patch \n"
+		fi
+	done
+
+	popd
+done
+
+printf "\n### APPLYING PERSONAL PATCHES ###\n";
+sleep 1.0;
+for path_personal in $(cd $personal; echo *); do
+	tree="$(tr _ / <<<$path_personal | sed -e 's;platform/;;g')"
+	printf "\n| $path_personal ###\n";
+	[ "$tree" == build ] && tree=build/make
+    [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
+    [ "$tree" == treble/app ] && tree=treble_app
+	pushd $tree
+
+	for patch in $personal/$path_personal/*.patch; do
+		# Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+            printf "### ALREDY APPLIED: $patch \n";
+			continue
+		fi
+
+		if git apply --check $patch; then
+			git am $patch
+		elif patch -f -p1 --dry-run < $patch > /dev/null; then
+			#This will fail
+			git am $patch || true
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
+		else
+			printf "### FAILED APPLYING: $patch \n"
+		fi
+	done
+
+	popd
+done
+
+printf "\n### APPLYING PONCES PATCHES ###\n";
+sleep 1.0;
+for path_ponces in $(cd $ponces; echo *); do
+	tree="$(tr _ / <<<$path_ponces | sed -e 's;platform/;;g')"
+	printf "\n| $path_ponces ###\n";
+	[ "$tree" == build ] && tree=build/make
+    [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
+    [ "$tree" == treble/app ] && tree=treble_app
+	pushd $tree
+
+	for patch in $ponces/$path_ponces/*.patch; do
+		# Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+            printf "### ALREDY APPLIED: $patch \n";
+			continue
+		fi
+
+		if git apply --check $patch; then
+			git am $patch
+		elif patch -f -p1 --dry-run < $patch > /dev/null; then
+			#This will fail
+			git am $patch || true
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
+		else
+			printf "### FAILED APPLYING: $patch \n"
+		fi
+	done
+
+	popd
+done
+
+printf "\n### APPLYING MATVEY PATCHES ###\n";
+sleep 1.0;
+for path_matvey in $(cd $matvey; echo *); do
+	tree="$(tr _ / <<<$path_matvey | sed -e 's;platform/;;g')"
+	printf "\n| $path_matvey ###\n";
+	[ "$tree" == build ] && tree=build/make
+    [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
+    [ "$tree" == treble/app ] && tree=treble_app
+	pushd $tree
+
+	for patch in $matvey/$path_matvey/*.patch; do
 		# Check if patch is already applied
 		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
             printf "### ALREDY APPLIED: $patch \n";
