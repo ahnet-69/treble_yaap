eccbf1a2f403e24c14da667980c2eae65a7adcc0
ahnet-69 <syedmusicrealise@gmail.com>
2023-06-25 13:04:51 +0500

    Fix MTK boot issues in YAAP.mk without lineage sepolicy patch

--- YAAP.mk
+++ YAAP.mk
@@ -4,7 +4,17 @@ $(call inherit-product, vendor/yaap/config/BoardConfigYAAP.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/yaap/build/core/config.mk
 
-TARGET_BOOTANIMATION_RES := 1080
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	persist.sys.binary_xml=false \
+	dalvik.vm.dex2oat64.enabled=true \
 
 PRODUCT_PACKAGES += \
   OpenEUICC \
